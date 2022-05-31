0bdf6a392fbde81a2e65501d8b67b6a65f5615d9
timothy1205 <timothy120500@gmail.com>
2022-05-31 02:00:34 -0600

    Added max_days to command-line argument

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import sys
 import docker_backup
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise RuntimeError("Enter directory for backups")
+    if len(sys.argv) < 3:
+        raise RuntimeError("Usage: main.py <backup_dir> <max_days>")
 
     PATH = sys.argv[1]
+    MAX_DAYS = int(sys.argv[2])
 
     docker_backup.MySQLBackup(PATH).execute()
     docker_backup.JellyfinBackup(PATH).execute()
@@ -17,4 +18,4 @@ if __name__ == "__main__":
     docker_backup.GrocyBackup(PATH).execute()
     docker_backup.DuplicatiBackup(PATH).execute()
 
-    docker_backup.delete_old_backups(PATH, 2)
+    docker_backup.delete_old_backups(PATH, MAX_DAYS)
