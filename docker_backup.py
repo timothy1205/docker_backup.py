a4434eeece278d06e732e3fee380e170fcdeaadf
timothy1205 <timothy120500@gmail.com>
2022-05-30 18:41:04 -0600

    Cleanup, comments

--- docker_backup.py
+++ docker_backup.py
@@ -1,22 +1,28 @@
 """
 Provides backup solutions for various types of containers
 """
-
-import docker
+import abc
 import gzip
 import os
 import datetime
+import docker
+from docker.models.containers import Container
 
-def merge_keywords(keywords, custom_keywords):
+def merge_keywords(keywords: list[str] | None, custom_keywords: list[str] | None):
+    """
+    Merge keywords lists or return other if one is None
+    """
     if custom_keywords is None:
         return keywords
     if keywords is None:
         return custom_keywords
 
-    print(keywords, custom_keywords)
     return keywords + custom_keywords
 
-def parse_env(container):
+def parse_env(container: Container):
+    """
+    Return dict of parsed envars for container
+    """
     unparsed_env = container.exec_run("env") \
         .output.decode("utf-8") \
         .split("\n")
@@ -33,10 +39,16 @@ def parse_env(container):
     return parsed
 
 def format_file_path(backup_dir: str, name: str, extension: str):
+    """
+    Append ISO8601 timestamp to given name/extension at backup_dir
+    """
     now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     return os.path.join(backup_dir, f'{name}_{now}.{extension}')
 
-def write_compressed_file(path, data):
+def write_compressed_file(path: str, data: bytes):
+    """
+    Compress and write data to path
+    """
     compressed_data = gzip.compress(data)
 
     with open(path, 'wb') as file:
@@ -45,7 +57,7 @@ def write_compressed_file(path, data):
     print(f'Wrote file: {path}')
 
 
-class BackupStrategy: # pylint: disable=too-few-public-methods
+class BackupStrategy(metaclass=abc.ABCMeta): # pylint: disable=too-few-public-methods
     """
     Abstract backup strategy
     """
@@ -58,10 +70,8 @@ class BackupStrategy: # pylint: disable=too-few-public-methods
 
         self.__filter_containers(keywords)
 
-
-
-    def __filter_containers(self, keywords):
-        def __helper(container):
+    def __filter_containers(self, keywords: list[str]):
+        def __helper(container: Container):
             for k in keywords:
                 if k in container.name:
                     return True
@@ -69,11 +79,13 @@ class BackupStrategy: # pylint: disable=too-few-public-methods
 
         self.containers = list(filter(__helper, self.containers))
 
+    @abc.abstractclassmethod
     def execute(self):
         """
         Execute backup solution
         """
         raise NotImplementedError()
+
 
 class MySQLBackup(BackupStrategy): # pylint: disable=too-few-public-methods
     """
@@ -85,7 +97,6 @@ class MySQLBackup(BackupStrategy): # pylint: disable=too-few-public-methods
         super().__init__(
             backup_dir, merge_keywords(self.keywords, keywords)
         )
-
 
     def execute(self):
         for container in self.containers:
